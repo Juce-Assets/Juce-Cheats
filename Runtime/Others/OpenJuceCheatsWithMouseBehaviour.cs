a708eef1488556eeec2d94d817375dfbe7e838f6
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-05-11 12:50:19 +0200

    Open behaviour

---
+++ Runtime/Others/OpenJuceCheatsWithMouseBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using Juce.Cheats.Core;
+using UnityEngine;
+
+namespace Juce.Cheats.Others
+{
+    public class OpenJuceCheatsWithMouseBehaviour : MonoBehaviour
+    {
+
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse2))
+            {
+                JuceCheats.Toggle();
+            }
+        }
+#endif
+
+    }
+}
